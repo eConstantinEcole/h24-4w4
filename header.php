f373d907167a39d7a03698ab95941721c2c387f8
eConstantinEcole <constantin.link1234@gmail.com>
2025-02-06 11:47:31 -0500

    Ajout de la gestion de la requête principale pour filtrer les articles de la catégorie 'nouvelle' sur la page d'accueil et création des fichiers d'en-tête et de pied de page.

---
+++ header.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>4W4-Voyage</title>
+    <?php wp_head() ?>  
+</head>
+<body>
+    <header>
+        <div class="entete">
+            <figure class="entete__logo">
+            <img src="<?php echo get_template_directory_uri() . '/images/logo.png' ?>" alt="logo voyage">
+
+            </figure>
+
+
+            <input type="checkbox" id="chk_burger" class="chk__burger">
+            <label for="chk_burger">
+                <img src="https://s2.svgbox.net/hero-outline.svg?ic=menu" alt="Menu" style="width: 32px; height: 32px;">
+            </label>
+
+
+
+
+
+            <div class="entete__nav">
+                <nav class="entete__menu">
+                    <ul class="menu">
+                        <li class="menu__li">
+                            <a class="menu__a" href="#">Aventure</a>
+                        </li>
+                        <li class="menu__li">
+                            <a class="menu__a" href="#">Sportive</a>
+                        </li>
+                        <li class="menu__li">
+                            <a class="menu__a"  href="#">Culturelle</a>
+                        </li>
+                        <li class="menu__li">
+                            <a  class="menu__a" href="#">Zen</a>
+                        </li>
+                        <li class="menu__li">
+                            <a class="menu__a" href="#">Sportive</a>
+                        </li>
+                        <li class="menu__li">
+                            <a class="menu__a"  href="#">Culturelle</a>
+                        </li>
+                    </ul>
+                </nav>
+                <div class="entete__recherche">
+                    <form  class="recherche">
+                        <input type="text" class="recherche__input">
+                        <img  class="recherche__img" src="https://s2.svgbox.net/hero-outline.svg?ic=search&color=000" width="20" height="20">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </header>
