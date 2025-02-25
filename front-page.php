df6857e6c8d3ad409f96d7604698d4cb0defae00
eConstantinEcole <constantin.link1234@gmail.com>
2025-02-25 11:05:26 -0500

    Ajout de nouveaux modèles de page et mise à jour des fonctions de thème pour améliorer la prise en charge des styles et des fonctionnalités

---
+++ front-page.php
@@ -0,0 +1,66 @@
+<?php
+/** 
+ * modèle front-page.php permet d'afficher la page d'accueil
+ * 
+*/
+?>
+
+<?php get_header() ?>
+<h1>front-page.php</h1>
+    <section class="hero">
+        <div class="hero__contenu global">
+            <h1 class="hero__titre">
+                <?php  bloginfo('name'); ?>
+            </h1>
+            <p class="hero__description">
+            <?php  bloginfo('description'); ?>
+            </p>
+            <a href="" class="hero__courriel">
+                info@cmaisonneuve.qc.ca
+            </a>
+            <button class="hero__bouton">
+                Inscription
+            </button>
+            <div class="hero__icone-app">
+                <img src="https://s2.svgbox.net/social.svg?ic=facebook&color=000000" width="20" height="20">
+                <img src="https://s2.svgbox.net/social.svg?ic=linkedin&color=000000" width="20" height="20">
+                <img src="https://s2.svgbox.net/social.svg?ic=paypal&color=000000" width="20" height="20">
+                <img src="https://s2.svgbox.net/social.svg?ic=stackoverflow&color=000000" width="20" height="20">
+            </div>
+        </div>
+    </section>
+    <section class="galerie">
+        <figure class="galerie__fig">
+            <img src="" alt="">
+        </figure>
+    </section>
+ 
+
+    <section class="populaire">
+        <div class="boiteflex global">
+            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+            <?php if (in_category('galerie')){
+                the_content();
+            } else { ?>         
+            <article class="carte carte--grande">
+                <figure class="carte__image">
+                    <img src="images/img1.jpg" alt="Image de voyage">
+                </figure>
+                <div class="carte__contenu">
+                    <?php
+                    if (has_post_thumbnail()) {
+                        // permet d'afficher la petite image associé à l'article (image mise en avant)
+                        the_post_thumbnail('thumbnail'); }
+                    ?>
+                    <h2 class="carte__titre"><?php the_title(); ?></h2>
+                    <p class="carte__description"><?php echo wp_trim_words(get_the_excerpt(), 20, "...") ; ?></p>
+                    <a class="carte__bouton carte__bouton--actif"  href="<?php the_permalink(); ?>">Suite</a>
+                </div>
+            </article>
+            <?php } ?>
+            <?php endwhile; endif; ?>
+        </div>
+    </section>
+    <?php get_footer(); ?>
+</body>
+</html>
--- functions.php
+++ functions.php
@@ -1,27 +1,21 @@
-<?php 
-// fonction php 
-?>
+<?php
 
+function mon_theme_supports() {
 
-<?php  
-function ajouter_css_dans_header() {
+  add_theme_support('title-tag');
+  add_theme_support('menus');
+  add_theme_support('post-thumbnails');
 
-  wp_enqueue_style(
-    'style_normalize',
-    get_template_directory_uri() . '/normalize.css',
-    array(),
-    filemtime(get_template_directory() . '/normalize.css'));
-
-
-    wp_enqueue_style(
-        'style_principal',
-        get_template_directory_uri() . '/style.css',
-        array(),
-        filemtime(get_template_directory() . '/style.css'));
 }
+add_action( 'after_setup_theme', 'mon_theme_supports' );
 
-add_action('wp_enqueue_scripts', 'ajouter_css_dans_header');
 
+
+function theme_tp_enqueue_styles() { 
+wp_enqueue_style('normalize', get_template_directory_uri() . '/css/normalize.css'); 
+wp_enqueue_style('main-style', get_stylesheet_uri()); 
+} 
+add_action('wp_enqueue_scripts', 'theme_tp_enqueue_styles');
 
 /**
  * Modifie la requete principale de WordPress avant qu'elle soit exécuté
@@ -30,13 +24,19 @@ add_action('wp_enqueue_scripts', 'ajouter_css_dans_header');
  * Dans ce cas ci nous filtrons la requête de la page d'accueil
  * @param WP_query  $query la requête principal de WP
  */
+
+
 function modifie_requete_principal( $query ) {
-  if ( $query->is_home() && $query->is_main_query() && ! is_admin() ) {
-    $query->set( 'category_name', 'nouvelle' );
-    $query->set( 'orderby', 'title' );
-    $query->set( 'order', 'ASC' );
-    }
-   }
-   add_action( 'pre_get_posts', 'modifie_requete_principal' );
+    if ( $query->is_home() && $query->is_main_query() && ! is_admin() ) {
+      $query->set( 'category_name', 'populaire' );
+      $query->set( 'orderby', 'title' );
+      $query->set( 'order', 'ASC' );
+      }
+     }
+     add_action( 'pre_get_posts', 'modifie_requete_principal' );
+
+
+
+
 
 ?>
