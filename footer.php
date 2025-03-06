cc3cbb83ca6ad2d2f0a1941a76344b79535ece22
eConstantinEcole <constantin.link1234@gmail.com>
2025-03-06 11:46:54 -0500

    Amélioration de la mise en page du footer : ajout d'un arrière-plan flou, réorganisation des éléments et mise à jour du style des titres et paragraphes.

--- footer.php
+++ footer.php
@@ -5,18 +5,20 @@
             <!-- ----------LIENS RAPIDES------------ -->
             <div class="piedpage__s1__externe">
               <h1>Menu</h1>
+              <p>
                 <?php wp_nav_menu(array(
                     "menu" => "externe",
                     "container" => "nav",
                     "container_class" => "piedpage__s1__externe__menu"
                 )); ?>
+                </p>
             </div>
 
             <!-- ---------CONTACT------------- -->
             <div class="piedpage__s1__adresse">
               <h1>Nous contacter</h1>
                 <div class="piedpage__s1__adresse__coord">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere porro veniam vitae, tempore corporis omnis nam 
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere porro veniam vitae, tempore corporis omnis nam </p>
                 </div>
                 <div class="piedpage__s1__adresse__recherche">
                     <?php get_search_form();   ?>
@@ -24,15 +26,14 @@
             </div>
 
             <!-- -----------MISSION DU CLUB----------- -->
-             <h1>La mission de notre club</h1>
-            <div class="piedpage__s1__description">
-                Le but de notre club est de permettre à tous les passionnés de la nature de se retrouver et de partager leur passion.
+             <div class="piedpage__s1__description">
+                <h1>La mission de notre club</h1>
+            
+                <p>Le but de notre club est de permettre à tous les passionnés de la nature de se retrouver et de partager leur passion.</p>
             </div>
         </section>
         <section class="piedpage__s2"></section>
         <section class="piedpage__s3"></section>
-
-
     </div>
 </footer>
 <?php wp_footer() ?>
