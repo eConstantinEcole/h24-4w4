bc5e4fbff0c3426807d1be7a5761bcde7c4b34fc
eConstantinEcole <constantin.link1234@gmail.com>
2025-03-18 18:21:55 -0400

    Nettoyage du code : suppression de la condition de catégorie dans la section populaire et ajout d'une nouvelle ligne à la fin du fichier SCSS principal.

--- front-page.php
+++ front-page.php
@@ -43,11 +43,9 @@
     <section class="populaire">
         <div class="boiteflex global">
             <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-            <?php if (in_category('galerie')){
-                the_content();
-            } else { ?>         
+            
             <?php get_template_part("gabarit/carte"); ?>
-            <?php } ?>
+            <?php  ?>
             <?php endwhile; endif; ?>
         </div>
     </section>
