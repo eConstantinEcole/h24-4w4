157cfc4eb874741ca5a6dff96c2ebc390a247342
eConstantinEcole <constantin.link1234@gmail.com>
2025-03-25 10:42:25 -0400

    Labo.04-Création du fichier 404.php

---
+++ 404.php
@@ -0,0 +1,10 @@
+<!-- Le modèle 404.php contiendra
+Un menu différent du menu principal
+Un titre
+Une description
+Les icône sociaux
+5 commits
+Utilisation du customizer
+intégration de l'entête et du footer
+Intégration de template-part -->
+
