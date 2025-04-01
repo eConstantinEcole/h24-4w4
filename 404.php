21fc2a2105dcd9f7aa0d3b7444ae23b934d07a81
eConstantinEcole <constantin.link1234@gmail.com>
2025-04-01 09:24:11 -0400

    Ajout Image Palmier

--- 404.php
+++ 404.php
@@ -1,12 +1,4 @@
-<!-- Le modèle 404.php contiendra
-Un menu différent du menu principal
-Un titre
-Une description
-Les icône sociaux
-5 commits
-Utilisation du customizer
-intégration de l'entête et du footer
-Intégration de template-part -->
+
 
 <?php get_header(); ?>
 
