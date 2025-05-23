3ec5d3575ce3a1c28c276c444929dbb889b3351b
eConstantinEcole <constantin.link1234@gmail.com>
2025-05-23 09:44:26 -0400

    Gallerie photos

--- template-pays.php
+++ template-pays.php
@@ -12,4 +12,11 @@ Template Name: Pays
     <?php echo wp_kses_post(get_theme_mod('pays_description', 'Texte de description par défaut pour la page Pays.')); ?>
 </p>
 
+<!-- Contenu de la page (galerie, blocs, etc.) -->
+<div class="contenu-page">
+    <?php the_content(); ?>
+</div>
+
+<div id="liste-articles"></div>
+
 <?php get_footer(); ?>
